48f6b3209d91611018996baf085e0f1206e462f9
RenatoFerreira <renatoyferreira@gmail.com>
2024-01-17 18:58:13 -0300

    create: Swagger routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, {json, Request, Response} from "express"
 import "express-async-errors"
 import httpStatus from "http-status"
+import swaggerUi from "swagger-ui-express"
+import swaggerdoc from "../swaggerdoc.json"
 import userRouter from "./routes/user-routes"
 import { errorHandler } from "./middleware/error-middleware"
 
@@ -8,9 +10,9 @@ const app = express()
 
 app.use(json())
 app.use(userRouter)
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerdoc))
 app.get("/health", (req: Request, res: Response) => {
-    res.status(httpStatus.OK).send("Tudo certo meu chapa!")
+    res.status(httpStatus.OK).send("Tudo certo com a api!")
 })
 app.use(errorHandler)
 
