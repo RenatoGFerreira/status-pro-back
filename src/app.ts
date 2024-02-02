231c32a78d2d35cc188949c5891f6358eed86229
RenatoFerreira <renatoyferreira@gmail.com>
2024-02-02 18:16:11 -0300

    fix: cors settings

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, {json, Request, Response} from "express";
+import cors from "cors"
 import "express-async-errors"
 import httpStatus from "http-status"
 import swaggerUi from "swagger-ui-express"
@@ -9,6 +10,7 @@ import { errorHandler } from "./middleware/error-middleware"
 const app = express()
 
 app.use(json())
+app.use(cors());
 app.use(userRouter)
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerdoc))
 app.get("/health", (req: Request, res: Response) => {
